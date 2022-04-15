64c9864c8067913ea7db274d547c3a0fa71b6185
Angel Choque <miguelangepacompia@gmail.com>
2022-04-14 23:49:22 -0500

    minor changes

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,21 +2,31 @@ import { useState } from "react"
 import useFetch from "@hooks/useFetch";
 import endPoints from "@pages/services/api";
 import Paginate from "@components/Paginate"
-const PRODUCT_LIMIT = 15
-const PRODUCT_OFFSET = 15
+import { Chart } from "@pages/common/Chart";
 
 export default function Dashboard() {
   
-  // const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET))
-  // console.log(products)
-  
-  
+  const PRODUCT_LIMIT = 15
   const [offsetProducts, setOffsetProducts] = useState(15)
   const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, offsetProducts));
   const totalProducts = useFetch(endPoints.products.getProducts(0, 0)).length;
 
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+  
+  const countOccurrences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+  const data = {
+    datasets: [{
+      label: "Categories",
+      data: countOccurrences(categoryCount),
+      borderWidht: 2,
+      backgroundColor: ["#ffbb11", "#C0C0C0", "#50AF95", "#F3BA2F", "#2A71D0"]
+    }]
+  }
   return (
     <>
+    <Chart className="mb-8 mt-2" chartData={data} />
     {totalProducts > 0 && <Paginate 
       totalItems={totalProducts}
       itemsPerPage={PRODUCT_LIMIT}
@@ -30,22 +40,34 @@ export default function Dashboard() {
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th 
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Name
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th 
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Category
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th 
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Price
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th 
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       ID
                     </th>
-                    <th scope="col" className="relative px-6 py-3">
+                    <th 
+                      scope="col"
+                      className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
-                    <th scope="col" className="relative px-6 py-3">
+                    <th 
+                      scope="col"
+                      className="relative px-6 py-3">
                       <span className="sr-only">Delete</span>
                     </th>
                   </tr>
